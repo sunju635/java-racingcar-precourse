a6eecf36c008df797d6927036611dbd997d46212
sunju635 <sunju5997@gmail.com>
2021-10-11 12:30:16 +0900

    feat($input): implement cars test
    BREAKING CHANGES:
    remove some test methods to move them to car which will be created later.

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,11 +1,13 @@
 package racinggame.domain;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Cars {
     private List<String> cars;
 
     public Cars(String cars) {
+        this.cars = Arrays.asList(cars.replaceAll(" ", "").split(","));
     }
 
     public String get(int i) {
--- src/test/java/racinggame/domain/CarsTest.java
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -2,18 +2,16 @@ package racinggame.domain;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racinggame.exception.GameErrorCode;
 import racinggame.exception.GameException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class CarsTest {
     Cars cars;
 
     @ParameterizedTest
     @ValueSource(strings = {"test1,test2", "test1", " test1, test2 "})
-    void Car_이름_분리_성공(String input) {
+    void Car_이름_분리(String input) throws GameException {
         cars = new Cars(input);
         String[] array = input.split(",");
 
@@ -23,26 +21,5 @@ public class CarsTest {
             assertThat(cars.get(i)).doesNotContain(" "); //공백 제거 확인
             assertThat(cars.get(i)).isEqualTo(array[i].replaceAll(" ", "")); //item 확인
         }
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"", " ", ",", " , "})
-    void Car_이름_분리_EMPTY_실패(String input) {
-        assertThatExceptionOfType(GameException.class).isThrownBy(
-                () -> {
-                    cars = new Cars(input);
-                }
-        ).withMessageContaining(GameErrorCode.CAR_NAME_EMPTY_ERROR.getErrorMessage());
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"test123"})
-    void Car_이름_분리_길이_실패(String input) {
-        assertThatExceptionOfType(GameException.class).isThrownBy(
-                () -> {
-                    cars = new Cars(input);
-                }
-        ).withMessageContaining(GameErrorCode.CAR_NAME_LENGTH_ERROR.getErrorMessage());
     }
 }
